404d0ce06490e227054264887f3b7c74dcbfecb6
Mahir Aziz <mahiraziz007@gmail.com>
2026-07-26 15:38:33 +0530

    minor fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 /* ROUTES */
 app.get("/", (req, res) => {
